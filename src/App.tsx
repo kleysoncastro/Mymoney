32b75af142df11fb96cd5c2fea41c612053a5f5f
kleyson castro <kleysoncastro99@gmail.com>
2021-08-26 16:50:16 -0400

    styles header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { Header } from "./components/Header";
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
-    <div className="app">
-      <h1>Hello world</h1>
+   <>
+      <Header />
 <GlobalStyles />
-    </div>
+    </>
 
   );
 }
